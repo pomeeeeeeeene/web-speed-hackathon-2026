0b20583bc2be83ecdca8e3ef103a8970fe0bdf95
pomeeeeeeeene <170702078+pomeeeeeeeene@users.noreply.github.com>
2026-03-20 13:15:11 +0900

    fix: respect process NODE_ENV in webpack environment plugin

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -73,7 +73,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || "",
-      NODE_ENV: "development",
+      NODE_ENV: process.env.NODE_ENV || "development",
     }),
     new MiniCssExtractPlugin({
       filename: "styles/[name].css",
